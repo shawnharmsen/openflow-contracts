ae99a664351b8212d9ef685f08559dd78360385b
x48 <96794053+x48115@users.noreply.github.com>
2023-05-19 00:16:02 -0500

    fix: dependency issue

--- test/Settlement.t.sol
+++ test/Settlement.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import {Settlement} from "../src/Settlement.sol";
-import {ISettlement} from "../../src/interfaces/ISettlement.sol";
+import {ISettlement} from "../src/interfaces/ISettlement.sol";
 import {Solver} from "../src/Solver.sol";
 import {SigUtils} from "../test/utils/SigUtils.sol";
 
