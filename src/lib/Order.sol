74973464b6da59af2d9f806201f4e1203ae0b379
x48 <96794053+x48115@users.noreply.github.com>
2023-06-06 01:55:04 -0500

    refactor: order lib comments

--- src/lib/Order.sol
+++ src/lib/Order.sol
@@ -1,13 +1,71 @@
-// SPDX-License-Identifier: BUSL 1.1
+// SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity 0.8.19;
-import "../interfaces/ISettlement.sol";
-import "../interfaces/IERC20.sol";
 
+/// @title Gnosis Protocol v2 Order Library
+/// @author Gnosis Developers
 library OrderLib {
+    /// @dev The byte length of an order unique identifier.
     uint256 internal constant _UID_LENGTH = 56;
 
-    // TODO: Move buildDigest and order methods here
+    /// @dev Packs order UID parameters into the specified memory location. The
+    /// result is equivalent to `abi.encodePacked(...)` with the difference that
+    /// it allows re-using the memory for packing the order UID.
+    ///
+    /// This function reverts if the order UID buffer is not the correct size.
+    ///
+    /// @param orderUid The buffer pack the order UID parameters into.
+    /// @param orderDigest The EIP-712 struct digest derived from the order
+    /// parameters.
+    /// @param owner The address of the user who owns this order.
+    /// @param validTo The epoch time at which the order will stop being valid.
+    function packOrderUidParams(
+        bytes memory orderUid,
+        bytes32 orderDigest,
+        address owner,
+        uint32 validTo
+    ) internal pure {
+        require(orderUid.length == _UID_LENGTH, "GPv2: uid buffer overflow");
 
+        // NOTE: Write the order UID to the allocated memory buffer. The order
+        // parameters are written to memory in **reverse order** as memory
+        // operations write 32-bytes at a time and we want to use a packed
+        // encoding. This means, for example, that after writing the value of
+        // `owner` to bytes `20:52`, writing the `orderDigest` to bytes `0:32`
+        // will **overwrite** bytes `20:32`. This is desirable as addresses are
+        // only 20 bytes and `20:32` should be `0`s:
+        //
+        //        |           1111111111222222222233333333334444444444555555
+        //   byte | 01234567890123456789012345678901234567890123456789012345
+        // -------+---------------------------------------------------------
+        //  field | [.........orderDigest..........][......owner.......][vT]
+        // -------+---------------------------------------------------------
+        // mstore |                         [000000000000000000000000000.vT]
+        //        |                     [00000000000.......owner.......]
+        //        | [.........orderDigest..........]
+        //
+        // Additionally, since Solidity `bytes memory` are length prefixed,
+        // 32 needs to be added to all the offsets.
+        //
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            mstore(add(orderUid, 56), validTo)
+            mstore(add(orderUid, 52), owner)
+            mstore(add(orderUid, 32), orderDigest)
+        }
+    }
+
+    /// @dev Extracts specific order information from the standardized unique
+    /// order id of the protocol.
+    ///
+    /// @param orderUid The unique identifier used to represent an order in
+    /// the protocol. This uid is the packed concatenation of the order digest,
+    /// the validTo order parameter and the address of the user who created the
+    /// order. It is used by the user to interface with the contract directly,
+    /// and not by calls that are triggered by the solvers.
+    /// @return orderDigest The EIP-712 signing digest derived from the order
+    /// parameters.
+    /// @return owner The address of the user who owns this order.
+    /// @return validTo The epoch time at which the order will stop being valid.
     function extractOrderUidParams(
         bytes memory orderUid
     )
@@ -20,20 +78,6 @@ library OrderLib {
             orderDigest := mload(add(orderUid, 32))
             owner := shr(96, mload(add(orderUid, 64)))
             validTo := shr(224, mload(add(orderUid, 84)))
-        }
-    }
-
-    function packOrderUidParams(
-        bytes memory orderUid,
-        bytes32 orderDigest,
-        address owner,
-        uint32 validTo
-    ) internal pure {
-        require(orderUid.length == _UID_LENGTH, "GPv2: uid buffer overflow");
-        assembly {
-            mstore(add(orderUid, 56), validTo)
-            mstore(add(orderUid, 52), owner)
-            mstore(add(orderUid, 32), orderDigest)
         }
     }
 }
