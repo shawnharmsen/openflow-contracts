5fb72a4be1808b5546602693ad87e6e7a512e523
x48 <96794053+x48115@users.noreply.github.com>
2023-06-15 16:46:01 -0500

    refactor: comment formatting

--- src/Settlement.sol
+++ src/Settlement.sol
@@ -17,10 +17,10 @@ import {OrderLib} from "./lib/Order.sol";
 /// - Order executor executes the swap in whatever way they see fit
 /// - At the end of the swap the user's `toToken` delta must be greater than or equal to the agreed upon `toAmount`
 contract Settlement is Signing {
-    /// @dev Use OrderLib for order UID encoding/decoding
+    /// @dev Use OrderLib for order UID encoding/decoding.
     using OrderLib for bytes;
 
-    /// @dev Prepare constants for building domainSeparator
+    /// @dev Prepare constants for building domainSeparator.
     bytes32 private constant _DOMAIN_NAME = keccak256("Blockswap");
     bytes32 private constant _DOMAIN_VERSION = keccak256("v0.0.1");
     bytes32 private constant _DOMAIN_TYPE_HASH =
@@ -33,7 +33,7 @@ contract Settlement is Signing {
         );
     bytes32 public immutable domainSeparator;
 
-    /// @dev Map each user order by UID to the amount that has been filled
+    /// @dev Map each user order by UID to the amount that has been filled.
     mapping(bytes => uint256) public filledAmount;
 
     /// @dev Contracts are allowed to submit pre-swap and post-swap hooks along with their order.
@@ -42,7 +42,7 @@ contract Settlement is Signing {
     /// primary contract upon which token allowances will be set.
     ExecutionProxy public executionProxy;
 
-    /// @dev When an order has been executed successfully emit an event
+    /// @dev When an order has been executed successfully emit an event.
     event OrderExecuted(
         address solver,
         address executor,
@@ -54,7 +54,7 @@ contract Settlement is Signing {
         uint256 toAmount
     );
 
-    /// @dev Set domainSeparator and executionProxy
+    /// @dev Set domainSeparator and executionProxy.
     constructor(address _orderManager) Signing(_orderManager) {
         domainSeparator = keccak256(
             abi.encode(
@@ -68,24 +68,24 @@ contract Settlement is Signing {
         executionProxy = new ExecutionProxy();
     }
 
-    /// @notice Primary method for order execution
+    /// @notice Primary method for order execution.
     /// @dev TODO: Analyze whether or not this needs to be non-reentrant
-    /// @param order The order to execute
+    /// @param order The order to execute.
     function executeOrder(ISettlement.Order calldata order) public {
         ISettlement.Payload memory payload = order.payload;
         address signatory = payload.sender;
 
-        /// @notice Step 1. Verify the integrity of the order
-        /// @dev Verifies that payload.sender signed the order
-        /// @dev Only the order payload is signed
-        /// @dev Once an order is signed anyone who has the signature can fulfil the order
-        /// @dev In the case of smart contracts sender must implement EIP-1271 isVerified method
+        /// @notice Step 1. Verify the integrity of the order.
+        /// @dev Verifies that payload.sender signed the order.
+        /// @dev Only the order payload is signed.
+        /// @dev Once an order is signed anyone who has the signature can fulfil the order.
+        /// @dev In the case of smart contracts sender must implement EIP-1271 isVerified method.
         bytes memory orderUid = _verify(order);
 
-        /// @notice Step 2. Execute optional contract pre-swap hooks
+        /// @notice Step 2. Execute optional contract pre-swap hooks.
         _execute(payload.sender, order.payload.hooks.preHooks);
 
-        /// @notice Step 3. Optimistically transfer funds from payload.sender to msg.sender (order executor)
+        /// @notice Step 3. Optimistically transfer funds from payload.sender to msg.sender (order executor).
         /// @dev Payload.sender must approve settlement.
         /// @dev If settlement already has `fromAmount` of `inputToken` send balance from Settlement.
         /// Otherwise send balance from payload.sender. The reason we do this is because the user may specify
@@ -103,19 +103,19 @@ contract Settlement is Signing {
             );
         }
 
-        /// @notice Step 4. Order executor executes the swap and is required to send funds to payload.recipient
-        /// @dev Order executors can be completely custom, or the generic order executor can be used
-        /// @dev Solver configurable metadata about the order is sent to the order executor hook
+        /// @notice Step 4. Order executor executes the swap and is required to send funds to payload.recipient.
+        /// @dev Order executors can be completely custom, or the generic order executor can be used.
+        /// @dev Solver configurable metadata about the order is sent to the order executor hook.
         /// @dev Settlement does not care how the solver executes the order, all Settlement cares about is that
-        /// the user receives the minimum amount of tokens the signer agreed to
+        /// the user receives the minimum amount of tokens the signer agreed to.
         /// @dev Record output token balance before so we can ensure recipient
-        /// received at least the agreed upon number of output tokens
+        /// received at least the agreed upon number of output tokens.
         uint256 outputTokenBalanceBefore = IERC20(payload.toToken).balanceOf(
             payload.recipient
         );
         ISolver(msg.sender).hook(order.data);
 
-        /// @notice Step 5. Make sure payload.recipient receives the agreed upon amount of tokens
+        /// @notice Step 5. Make sure payload.recipient receives the agreed upon amount of tokens.
         uint256 outputTokenBalanceAfter = IERC20(payload.toToken).balanceOf(
             payload.recipient
         );
@@ -124,7 +124,7 @@ contract Settlement is Signing {
         require(balanceDelta >= payload.toAmount, "Order not filled");
         filledAmount[orderUid] = balanceDelta;
 
-        /// @notice Step 6. Execute optional contract post-swap hooks
+        /// @notice Step 6. Execute optional contract post-swap hooks.
         /// @dev These are signer authenticated post-swap hooks. These hooks
         /// happen after step 5 because the user may wish to perform an action
         /// (such as deposit into a vault or reinvest/compound) with the swapped funds.
@@ -143,8 +143,8 @@ contract Settlement is Signing {
         );
     }
 
-    /// @notice Pass hook execution interactions to execution proxy to be executed
-    /// @param interactions The interactions to execute
+    /// @notice Pass hook execution interactions to execution proxy to be executed.
+    /// @param interactions The interactions to execute.
     function _execute(
         address signatory,
         ISettlement.Interaction[] memory interactions
@@ -154,16 +154,17 @@ contract Settlement is Signing {
         }
     }
 
-    /// @notice Order verification
-    /// @dev Verify the order
-    /// @dev Signature type is auto-detected based on signature's v
-    /// see: Gnosis Safe implementationa.
+    /// @notice Order verification.
+    /// @dev Verify the order.
+    /// @dev Signature type is auto-detected based on signature's v.
+    /// see: Gnosis Safe implementation.
     /// @dev Supports:
     /// - EIP-712 (Structured EOA signatures)
     /// - EIP-1271 (Contract based signatures)
     /// - EthSign (Non-structured EOA signatures)
-    /// @param order Complete signed order
-    /// @return orderUid New order UID
+    /// - Presign (Anyone can presign a digest)
+    /// @param order Complete signed order.
+    /// @return orderUid New order UID.
     function _verify(
         ISettlement.Order calldata order
     ) internal view returns (bytes memory orderUid) {
@@ -176,8 +177,8 @@ contract Settlement is Signing {
         require(block.timestamp <= order.payload.deadline, "Deadline expired");
     }
 
-    /// @notice Building the digest hash
-    /// @dev Message digest hash consists of type hash, domain separator and struct hash
+    /// @notice Building the digest hash.
+    /// @dev Message digest hash consists of type hash, domain separator and struct hash.
     function buildDigest(
         ISettlement.Payload memory _payload
     ) public view returns (bytes32 orderDigest) {
@@ -191,28 +192,28 @@ contract Settlement is Signing {
     }
 }
 
-/// @title Execution proxy
-/// @notice Simple contract used to execute pre-swap and post-swap hooks
+/// @title Execution proxy.
+/// @notice Simple contract used to execute pre-swap and post-swap hooks.
 /// @dev This is necessary because we cannot allow Settlement to execute arbitrary transaction
 /// payloads directly since Settlement may have token approvals.
 contract ExecutionProxy {
     address public immutable settlement;
 
-    /// @dev Set settlement address
+    /// @dev Set settlement address.
     constructor() {
         settlement = msg.sender;
     }
 
-    /// @notice Executed user defined interactions signed by sender
-    /// @dev Sender has been authenticated by signature recovery
+    /// @notice Executed user defined interactions signed by sender.
+    /// @dev Sender has been authenticated by signature recovery.
     /// @dev Something very important to consider here is that we are appending
-    /// the authenticated sender  (signer) to the end of each interaction calldata.
+    /// the authenticated sender (signer) to the end of each interaction calldata.
     /// The reason this is done is to allow the payload signatory to be
     /// authenticated in interaction endpoints. If your interaction endpoint
     /// needs to read signer it can do so by reading the last 20 bytes of calldata.
     /// What this means is that if your interaction endpoint explicitly relies on
     /// calldata length you will need to account for the additional 20 address bytes.
-    /// For example: signatory := shr(96, calldataload(sub(calldatasize(), 20)))
+    /// For example: signatory := shr(96, calldataload(sub(calldatasize(), 20))).
     function execute(
         address sender,
         ISettlement.Interaction[] memory interactions
