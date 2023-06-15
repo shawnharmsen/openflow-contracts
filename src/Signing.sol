2765b0212f1ea7e20cc88545fec817a31cc55178
x48 <96794053+x48115@users.noreply.github.com>
2023-06-15 16:28:14 -0500

    refactor: signatures and comments

--- src/Signing.sol
+++ src/Signing.sol
@@ -175,19 +175,22 @@ contract Signing {
             signature.length == _ECDSA_SIGNATURE_LENGTH,
             "Malformed ECDSA signature"
         );
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-        assembly {
-            r := mload(add(signature, 0x20))
-            s := mload(add(signature, 0x40))
-            v := and(mload(add(signature, 0x41)), 0xff)
-        }
+        (uint8 v, bytes32 r, bytes32 s) = _signatureSplit(signature, 0);
         signer = ecrecover(message, v, r, s);
         require(signer != address(0), "Invalid ECDSA signature");
     }
 
-    function signatureSplit(
+    /// @notice Splits signature bytes into `uint8 v, bytes32 r, bytes32 s`.
+    /// @dev Make sure to perform a bounds check for @param pos, to avoid out of bounds access on @param signatures
+    /// The signature format is a compact form of {bytes32 r}{bytes32 s}{uint8 v}
+    /// Compact means uint8 is not padded to 32 bytes.
+    /// @param pos Which signature to read.
+    /// A prior bounds check of this parameter should be performed, to avoid out of bounds access.
+    /// @param signatures Concatenated {r, s, v} signatures.
+    /// @return v Recovery ID or Safe signature type.
+    /// @return r Output value r of the signature.
+    /// @return s Output value s of the signature.
+    function _signatureSplit(
         bytes memory signatures,
         uint256 pos
     ) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
@@ -196,27 +199,36 @@ contract Signing {
             let signaturePos := mul(0x41, pos)
             r := mload(add(signatures, add(signaturePos, 0x20)))
             s := mload(add(signatures, add(signaturePos, 0x40)))
+            /// @dev Here we are loading the last 32 bytes, including 31 bytes of 's'.
+            /// There is no 'mload8' to do this. 'byte' is not working due to
+            /// the Solidity parser, so lets use the second best option, 'and'.
             v := and(mload(add(signatures, add(signaturePos, 0x41))), 0xff)
         }
     }
 
-    /// @notice Gnosis style signature threshold check
-    /// @param orderManager The address responsible for signer storage and order management
-    /// @param digest The digest to check signatures for
-    /// @param signatures Packed and encoded multisig signatures payload
-    // / @param requiredSignatures Signature threshold. This is required since we are unable
-    /// to easily determine the number of signatures from the signature payload alone
-    /// @dev Reverts if signature threshold is not passed
+    /// @notice Gnosis style signature threshold check.
+    /// @dev Since the EIP-1271 does an external call, be mindful of reentrancy attacks.
+    /// @dev Reverts if signature threshold is not passed.
+    /// @dev Signatures must be packed such that the decimal values of the derived signers are in ascending numerical order.
+    /// For instance `0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48` > `0x6B175474E89094C44Da98b954EedeAC495271d0F`, so the
+    /// signatuer for `0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48` must come first.
     /// @dev Code comes from Gnosis Safe: https://github.com/safe-global/safe-contracts/blob/main/contracts/Safe.sol
-    /// The only change is that we use recoverSigner to calculate signer based on v instead of doing this inline.
-    /// The reason for this is that it lets us recover one signer at a time or multiple signers
-    /// at a time with maximum  with maximum flexibility
+    /// The only change is that we use `recoverSigner()` to calculate signer based on v instead of doing this inline.
+    /// The reason for this is that it lets us recover one signer at a time or multiple signers at a
+    /// time with maximum  with maximum flexibility.
+    /// @param digest The EIP-712 signing digest derived from the order parameters.
+    /// @param signatures Packed and encoded multisig signatures payload.
+    /// @param requiredSignatures Signature threshold. This is required since we are unable.
+    /// to easily determine the number of signatures from the signature payload alone.
     function checkNSignatures(
         bytes32 digest,
         bytes memory signatures,
         uint256 requiredSignatures
     ) public view {
+        /// @dev Check that the provided signature data is not too short
         require(signatures.length >= requiredSignatures * 65, "GS020");
+
+        /// @dev There cannot be an owner with address 0.
         address lastOwner = address(0);
         address currentOwner;
         uint256 signatureIdx;
@@ -228,15 +240,8 @@ contract Signing {
             signatureIdx < requiredSignatures;
             signatureIdx++
         ) {
-            bytes memory signature;
-            // From Gnosis signatureSplit method
-            assembly {
-                let signaturePos := mul(0x41, signatureIdx)
-                r := mload(add(add(signatures, signaturePos), 0x20))
-                s := mload(add(add(signatures, signaturePos), 0x40))
-                v := and(mload(add(add(signatures, signaturePos), 0x41)), 0xff)
-            }
-            signature = abi.encodePacked(r, s, v);
+            (v, r, s) = _signatureSplit(signatures, signatureIdx);
+            bytes memory signature = abi.encodePacked(r, s, v);
             currentOwner = recoverSigner(digest, signature);
             require(
                 currentOwner > lastOwner,
