7a7d6ef33d43fa8da0828c481269e4ac1451dab3
x48 <96794053+x48115@users.noreply.github.com>
2023-06-17 11:59:18 -0500

    refactor: more coverage

--- test/Signing.t.sol
+++ test/Signing.t.sol
@@ -2,8 +2,16 @@
 pragma solidity 0.8.19;
 import "./support/Storage.sol";
 
+contract NegativeTestCaseSigningContract {
+    function isValidSignature(
+        bytes32 digest,
+        bytes calldata signatures
+    ) external returns (bytes4) {}
+}
+
 contract SigningTest is Storage {
     bytes32 digest = bytes32(hex"1337");
+    bytes32 badDigest = bytes32(hex"deadbeef");
 
     function testEip1271() external {
         bytes memory signature1 = _sign(_USER_A_PRIVATE_KEY, digest);
@@ -15,7 +23,7 @@ contract SigningTest is Storage {
             strategy,
             _sign(
                 _USER_A_PRIVATE_KEY,
-                bytes32(hex"deadbeef") // Sign a random digest
+                badDigest // Sign a random digest
             ),
             signature2
         );
@@ -43,20 +51,46 @@ contract SigningTest is Storage {
             digest,
             encodedSignatures
         );
+
+        /// @dev Invalid ECDSA signature
+        vm.expectRevert("Invalid ECDSA signature");
+        encodedSignatures = abi.encodePacked(strategy, signatures);
+        bytes memory signatureInvalid;
+        assembly {
+            mstore(signatureInvalid, 65) // 65 empty bytes
+        }
+        settlement.recoverSigner(
+            ISettlement.Scheme.Eip712,
+            digest,
+            signatureInvalid
+        );
+
+        /// @dev Signature not valid
+        address negativeTestCaseSigningContract = address(
+            new NegativeTestCaseSigningContract()
+        );
+        encodedSignatures = abi.encodePacked(
+            negativeTestCaseSigningContract,
+            signatures
+        );
+        vm.expectRevert("EIP-1271 signature is invalid");
+        settlement.recoverSigner(
+            ISettlement.Scheme.Eip1271,
+            digest,
+            encodedSignatures
+        );
     }
 
     function testEip712() external {
         /// @dev Malformed ECDSA signature.
         bytes memory signature1 = _sign(_USER_A_PRIVATE_KEY, digest);
         assembly {
-            mstore(signature1, 66) // Set to 66 bytes instead of 65
+            mstore(signature1, 66) // Set signature to 66 bytes instead of 65
         }
         vm.expectRevert("Malformed ECDSA signature");
         settlement.recoverSigner(ISettlement.Scheme.Eip712, digest, signature1);
-
-        /// @dev Valid signature.
         assembly {
-            mstore(signature1, 65) // Set back to 65
+            mstore(signature1, 65) // Set signature back to 65 bytes
         }
         settlement.recoverSigner(ISettlement.Scheme.Eip712, digest, signature1);
     }
