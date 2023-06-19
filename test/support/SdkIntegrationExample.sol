bc071adcff4b7057ed1f7180b94707c0abfabe7f
x48 <96794053+x48115@users.noreply.github.com>
2023-06-19 18:32:56 -0500

    refactor: sdk options updating

--- test/support/SdkIntegrationExample.sol
+++ test/support/SdkIntegrationExample.sol
@@ -4,20 +4,26 @@ import {IERC20} from "../../src/interfaces/IERC20.sol";
 import "../../src/interfaces/IOpenflow.sol";
 
 contract SdkIntegrationExample {
+    /// @notice Initialize SDK instance variable.
     IOpenflowSdk public sdk;
 
+    /// @notice Create a new SDK instance.
     constructor(address _openflowFactory) {
-        address sdkInstanceManager = msg.sender;
+        address sdkInstanceManager = address(this);
         sdk = IOpenflowFactory(_openflowFactory).newSdkInstance(
             sdkInstanceManager
         );
     }
 
+    /// @notice Execute a basic swap.
+    /// @dev Note: This method has no auth. If your app needs auth make sure to add it.
     function swap(address fromToken, address toToken) external {
         IERC20(fromToken).approve(address(sdk), type(uint256).max);
         sdk.swap(fromToken, toToken);
     }
 
+    /// @notice Update SDK options.
+    /// @dev Note: This method has no auth. If your app needs auth make sure to add it.
     function updateOptions() external {
         IOpenflowSdk.Options memory options = sdk.options();
         options.auctionDuration = 60 * 5; // Set auction duration to 5 Minutes.
